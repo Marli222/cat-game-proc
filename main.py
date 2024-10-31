76bbae4d250954cca95d5b1aea2e87156233ae87
Marli222 <marli.williams@laetottenham.ac.uk>
2024-10-31 14:28:40 +0000

    Add statements

--- main.py
+++ main.py
@@ -1,27 +1,43 @@
 cat_attributes = {
     "intelligence": 0,
-    "energy": 0,
-    "weight": 0,
-    # change the inital values above
+    "energy": 10,
+    "weight": 4,
 }
 
-print("Welcome to my cat game!!")
+print("Welcome to my cat game!!!")
 
 # Take the user inputs for name and colour:
-name = input("Enter name:")
-# ...
+name = input("Enter name: ")
+colour = input("Enter colour: ")
 
 # start the while loop
 while True:
     # Finish the string below
-    option = input("What would you like to do? 1. Play with your cat 2. Train your cat 3. show stats")
-
+    option = input("""What would you like to do? 
+                   1. Play with your cat 
+                   2. Train your cat 
+                   3. Show stats
+                   4. Feed your cat
+                   5. Put your cat to sleep
+                   """)
     if option == '1':
-        # change the cat's attributes here
+        print("You played with your cat!")
+        cat_attributes['energy'] -= 1
         pass
     elif option == '2':
-        pass
-    # elif ...
+        print("You trained your cat!")
+        cat_attributes['intelligence'] += 1
+    elif option == '3':
+        print("Your Stats:")
+        for x in cat_attributes:
+            print(f"Your cat's {x} is {cat_attributes[x]}")
+    elif option == '4':
+        print("You feed your cat!")
+        cat_attributes['weight'] += 1
+        cat_attributes['energy'] += 1
+    elif option == '5':
+        print("Your cat is asleep!")
+        cat_attributes['energy'] -= 1
     else:
         pass
 
